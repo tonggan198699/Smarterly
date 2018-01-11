1cfc0450a71768d55f87d8c8e1c3edc2df79e212
tonggan198699 <gantong1986@gmail.com>
2018-01-11 21:49:55 +0000

    Skipping task 1

--- php/index.php
+++ php/index.php
@@ -1,11 +1,24 @@
+<html lang="en">
+  <head>
+    <title>Task 1</title>
+  </head>
+  <body>
+
+<table border=1>
+
+  <tr>
+     <th>category</th>
+     <th>type</th>
+     <th>location</th>
+     <th>outcode</th>
+   </tr>
+
 <?php
-
 # Our Current Location
-$location = ['51.301819','-0.337613'];
+$location = ['51.301819','-0.337613']; # longitude and latitude
 
-$link = mysqli_connect("localhost","root","","postboxes");
+$link = mysqli_connect("localhost","root","root","postboxes");
 $result = $link->query("SELECT * FROM boxes WHERE lat LIKE '%51%' AND lng LIKE '%-0.33%' LIMIT 10");
-
 $output = "<table>";
 while($row = mysqli_fetch_array($result)) {
   $output .= "<tr>";
@@ -16,7 +29,12 @@ while($row = mysqli_fetch_array($result)) {
     $output .= "<td>".$row['outcode']."</td>";
     $output .= "<td>Collected At: ".$row['collection']."</td>";
   $output .= "</tr>";
-
 }
 
 echo $output;
+
+?>
+
+</body>
+
+</html>
